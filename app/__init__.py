90535f3af9fc5bc36cc380d91ff2d404fb90aa58
navu545 <navu545@gmail.com>
2026-01-30 17:38:06 +0530

    Implement webhook receiver, MongoDB integration and UI

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,23 @@
 from flask import Flask
-
 from app.webhook.routes import webhook
+from app.extensions import mongo
 
 
 # Creating our flask app
 def create_app():
 
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="../ui")
+    
+    #test route
+    @app.route('/')
+    def api_root():
+        return 'Welcome guys'
+    
+    #Establish connection with mongoDB
+    app.config["MONGO_URI"] = "mongodb://localhost:27017/github_events"
+
+    #DB to app connection
+    mongo.init_app(app)
     
     # registering all the blueprints
     app.register_blueprint(webhook)
--- app/webhook/routes.py
+++ app/webhook/routes.py
@@ -1,7 +1,79 @@
-from flask import Blueprint, json, request
+from flask import Blueprint, jsonify, request, render_template
+from datetime import datetime, timezone
+from app.extensions import mongo
 
 webhook = Blueprint('Webhook', __name__, url_prefix='/webhook')
 
+
 @webhook.route('/receiver', methods=["POST"])
+
+
 def receiver():
-    return {}, 200
+
+    if not payload:
+        return jsonify({"msg": "invalid payload"}), 400
+    
+    payload = request.json
+    event_type = request.headers.get("X-GitHub-Event")
+
+    events = mongo.db.events
+
+    timestamp = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S UTC")
+
+    if event_type == "push":
+        data = {
+            "request_id": payload["after"],
+            "author": payload["pusher"]["name"],
+            "action": "PUSH",
+            "from_branch": None,
+            "to_branch": payload["ref"].split("/")[-1],
+            "timestamp": timestamp
+        }
+
+    elif event_type == "pull_request":
+        pr = payload["pull_request"]
+
+        if pr.get("merged"):
+             
+            data = {
+                "request_id": str(pr["id"]),
+                "author": pr["merged_by"]["login"],
+                "action": "MERGE",
+                "from_branch": pr["head"]["ref"],
+                "to_branch": pr["base"]["ref"],
+                "timestamp": timestamp
+            }   
+
+        else:
+            data = {
+                "request_id": str(pr["id"]),
+                "author": pr["user"]["login"],
+                "action": "PULL_REQUEST",
+                "from_branch": pr["head"]["ref"],
+                "to_branch": pr["base"]["ref"],
+                "timestamp": timestamp
+            }   
+
+    else:
+        return jsonify({"msg": "ignored"}), 200
+
+    events.insert_one(data)
+    return jsonify({"msg":"stored"}), 200
+
+
+@webhook.route("/events", methods=["GET"])
+def get_events():
+    events = mongo.db.events.find().sort("timestamp", -1).limit(10)
+
+    result = []
+    for e in events:
+        e["_id"] = str(e["_id"])
+        result.append(e)
+
+    return jsonify(result)    
+
+@webhook.route("/", methods=["GET"])
+def home():
+    return render_template("index.html")
+
+
